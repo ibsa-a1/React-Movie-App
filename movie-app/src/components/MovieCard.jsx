51368a0a64fbd6593a17c2309339de643ea4cff4
ibsa-a1 <ibsaabera712@gmail.com>
2025-07-15 16:12:10 +0300

    favorite-btn and transform issue fixed

--- movie-app/src/components/MovieCard.jsx
+++ movie-app/src/components/MovieCard.jsx
@@ -17,7 +17,7 @@ function MovieCard({movie}) {
       <div className='movie-poster'>
         <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
         <div className='movie-overlay'>
-          <button className={`favorite-btn ${favorite ? 'active' : ''}`} onClick={onFavoriteClick}>🤍</button>
+          <button className={`favorite-btn ${favorite ? 'active' : ''}`} onClick={onFavoriteClick}>♥</button>
         </div>
       </div>
       <div className='movie-info'>
