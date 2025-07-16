be94003287808ff7b4240b4836baf3882467aa91
ibsa-a1 <ibsaabera712@gmail.com>
2025-07-16 12:05:13 +0300

    rating added

--- movie-app/src/components/MovieCard.jsx
+++ movie-app/src/components/MovieCard.jsx
@@ -22,6 +22,7 @@ function MovieCard({movie}) {
       </div>
       <div className='movie-info'>
         <h3>{movie.title}</h3>
+        <p> ⭐ {movie.vote_average.toFixed(1)} </p>
         <p>{movie.release_date?.split("-")[0]}</p>
       </div>
     </div>
