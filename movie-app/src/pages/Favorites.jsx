d1126ee21c766ceeda1910ae80ad550a37aa43a9
ibsa-a1 <ibsaabera712@gmail.com>
2025-07-25 21:20:10 +0300

    fix favorites order

--- movie-app/src/pages/Favorites.jsx
+++ movie-app/src/pages/Favorites.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import '../css/Favorites.css'
-import { useMovieContext } from '../contexts/MovieContext'
-import MovieCard from '../components/MovieCard'
+import React from "react";
+import "../css/Favorites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
 
 function Favorites() {
   const { favorites } = useMovieContext();
@@ -11,20 +11,20 @@ function Favorites() {
       <div className="favorites">
         <h2>Your Favorites</h2>
         <div className="movies-grid">
-          {favorites.map((movie) => (
+          {[...favorites].reverse().map((movie) => (
             <MovieCard movie={movie} key={movie.id} />
           ))}
         </div>
       </div>
     );
   }
-  
+
   return (
-    <div className='favorites-empty'>
+    <div className="favorites-empty">
       <h2>No favorites yet</h2>
       <p>Add some movies to your favorites</p>
     </div>
-  )
+  );
 }
 
-export default Favorites
+export default Favorites;
